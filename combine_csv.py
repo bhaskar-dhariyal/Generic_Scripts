db06cba0d31039efc9d33646a5517d738f298e8d
Bhaskar Dhariyal <bdhariyal94@gmail.com>
2020-03-02 18:00:58 +0000

    Add file name option

--- combine_csv.py
+++ combine_csv.py
@@ -1,23 +1,36 @@
+#**************************************************************
+#*                                                            *
+#*                                                            *
+#* combine_csv.py                                             *
+#*                                                            *
+#* By: Bhaskar Dhariyal <bhaskar.dhariyal@ucdconnect.ie>      *
+#*                                                            *
+#* created: 03/02/20 16:16:34 by Bhaskar Dhariyal             *
+#* updated: 03/02/20 17:57:06 by Bhaskar Dhariyal             *
+#*                                                            *
+#*                                                            *
+#***********************************************************²**
+
 import os
 import glob
 import pandas as pd
 import click
-#set working directory
-
 
 @click.command()
-@click.option('--path', help="Path of CSV files", required=True, type=click.Path(exists=True))
-def combine(path):
+@click.argument('path', required=True, type=click.Path(exists=True), nargs = 1)
+@click.argument('file_name', nargs = 1)
+def combine(path, file_name):
     os.chdir(path)
 
-    all_filenames = [i for i in glob.glob('*.{}'.format('csv'))]
-    #print(all_filenames)
+    all_filenames = sorted(i for i in glob.glob('*.{}'.format('csv')))
 
-    #combine all files in the list
     combined_csv = pd.concat([pd.read_csv(f) for f in all_filenames ])
-    #export to csv
-    combined_csv.to_csv( "combined_csv.csv", index=False, encoding='utf-8-sig')
-    print(f"Finished combining")
+    combined_csv.to_csv(f"{file_name}.csv", index=False, encoding='utf-8-sig')
+    print("Finished combining")
 
+    print("Deleting temporary files")
+    #for item in all_filenames:
+    #    os.remove(f"{item}.csv")
+        
 if __name__ == "__main__":
     combine()
